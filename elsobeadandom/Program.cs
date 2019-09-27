4592fe0307f8efe932155359c29b6c8d5323af7a
Máté Odler <odlermate@gmail.com>
2019-09-27 11:14:24 +0200

    1. feladat kész

--- elsobeadandom/Program.cs
+++ elsobeadandom/Program.cs
@@ -10,7 +10,18 @@ namespace elsobeadandom
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            var szamok = new List<int>();
+            Console.WriteLine("Most adj meg 10 számot kérlek: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Kérlek add meg a(z) {0}. számot ", i +1);
+                szamok.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            int max = szamok.Max();
+            Console.WriteLine("A legnagyobb szám: " + max);
+
+
+            Console.ReadKey();
         }
     }
 }
